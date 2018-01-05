d6bcd1dbe83ab18ca6a078ca830dc062c30b12a8
nikejune <dmlgus322@hanyang.ac.kr>
2018-01-05 17:38:09 +0900

    Initial Commit

---
+++ practice_git/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  practice_git
+//
+//  Created by nikejune on 2018. 1. 5..
+//  Copyright © 2018년 nikejune. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
